2f439c7fa3764a401a45186be5b2d2496d2f8ae2
dkhilchuk <dkhilchuk@n-ix.com>
2020-04-16 10:02:46 +0300

    added edit post

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,23 +1,48 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Post } from '../post.model';
 import { NgForm } from '@angular/forms';
 import { PostService } from '../post.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.scss']
 })
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit {
+  post: Post;
+  postId: string;
+  private mode: string = 'create';
+
   constructor(
-    private postService: PostService
+    private postService: PostService,
+    public activatedRoute: ActivatedRoute
   ) {
 
   }
 
-  onAddPost(form: NgForm) {
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.post = this.postService.getPost(this.postId);
+      } else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
+  }
+
+  onSavePost(form: NgForm) {
     if (form.valid) {
-      this.postService.addPost(form.value.title, form.value.content);
+      if (this.mode === 'create') {
+        this.postService.addPost(form.value.title, form.value.content);
+      } else {
+        console.info(form.value.title, form.value.content);
+        this.postService.updatePost(this.postId, form.value.title, form.value.content);
+      }
+
       form.resetForm();
     }
   }
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -32,8 +32,21 @@ export class PostService {
       });
   }
 
+  getPost(id: string) {
+    return {...this.posts.find(post => post.id === id)};
+  }
+
   getPostsUpdateListened() {
     return this.postsUpdated.asObservable();
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = { id, title, content };
+
+    this.http.put(`http://localhost:3000/api/${id}`, post)
+      .subscribe((res) => {
+        console.info(res);
+      });
   }
 
   addPost(title: string, content: string) {

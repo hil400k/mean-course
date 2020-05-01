c47d31ac21c2c7ff1d0053f99037e47b22b18aea
dkhilchuk <dkhilchuk@n-ix.com>
2020-05-01 13:14:30 +0300

    handling of expiration

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -52,7 +52,7 @@ router.post("/login", (req, res, next) => {
     }, "secret-this-should-be-longer", {
       expiresIn: "1h"
     })
-    res.status(200).json({ token })
+    res.status(200).json({ token, expiresIn: 3600 })
   }).catch((err) => {
     return res.status(401).json({
       message: "Auth failed"
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -10,6 +10,8 @@ import { Router } from '@angular/router';
 export class AuthService {
   private isAuthenticated = false;
   private token: string;
+  // @ts-ignore
+  private tokenTimer: NodeJS.Timer;
   private authStatusListener = new Subject<boolean>();
 
   constructor(
@@ -46,10 +48,14 @@ export class AuthService {
       email,
       password
     };
-    this.http.post<{ token: string }>('http://localhost:3000/user/login', authData)
+    this.http.post<{ token: string, expiresIn: number }>('http://localhost:3000/user/login', authData)
       .subscribe((response) => {
         this.token = response.token;
         if (this.token) {
+          const expiresInDuration = response.expiresIn;
+          this.tokenTimer = setTimeout(() => {
+            this.logout();
+          }, expiresInDuration * 1000);
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
           this.router.navigate(['/']);
@@ -61,6 +67,7 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
+    clearTimeout(this.tokenTimer);
     this.router.navigate(['/']);
   }
 }
